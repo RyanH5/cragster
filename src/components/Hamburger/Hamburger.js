b24fe0775a3305e8a85020a6bd0b688de4c0898d
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-10-24 15:24:10 -0600

    Create responsive hamburger style header template

---
+++ src/components/Hamburger/Hamburger.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Hamburger.css';
+
+export const Hamburger = () => {
+  return (
+    <nav>
+         <input type="checkbox" id="css-toggle-menu" name="css-toggle-menu" /> 
+         <ul class="main">
+               <li><a href="">Home</a></li>
+               <li><a href="">Services</a></li>
+               <li><a href="">Contact</a></li>
+               <li><a href="">Login</a></li>
+               <li><a href="">Sign up</a></li>
+               <li><a href="">Settings</a></li>
+         </ul> 
+         <label for="css-toggle-menu" id="css-toggle-menu"></label> 
+   </nav>
+  )
+}
